4b80919147986443fca4300492c7ab7e31451d0b
Komborerai Chikweshe <kombogc@gmail.com>
2024-03-19 22:14:06 +0200

    comment out security deps

--- backend/src/auth/router.py
+++ backend/src/auth/router.py
@@ -4,7 +4,7 @@ from auth.models import UserLoginSchema, UserSignupSchema
 from models.users import User
 from datetime import datetime
 from database import db
-from auth.utils import hash_password, verify_password, create_access_token
+# from auth.utils import hash_password, verify_password, create_access_token
 
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
@@ -18,11 +18,11 @@ async def login(user: UserLoginSchema = Body(...)):
         return {"error": "users does not exists."}
 
     user = user.dict()
-    if verify_password(user.get("password"), registered.get("password")):
-        token = create_access_token(user["email"])
-        return {"access_token": token}
-    else:
-        return {"error", "password is incorrect."}
+    # if verify_password(user.get("password"), registered.get("password")):
+    #     token = create_access_token(user["email"])
+    #     return {"access_token": token}
+    # else:
+    #     return {"error", "password is incorrect."}
 
 
 
@@ -36,10 +36,10 @@ async def signup(user: UserSignupSchema = Body(...)):
     if user.password != user.password_confirmation:
         return { "error": "Passwords are different" }
     
-    hashed = hash_password(user.password)
+    # hashed = hash_password(user.password)
     new_user = {
         "email": user.email,
-        "password": hashed,
+        "password": "hashed",
         "created_at": datetime.utcnow(),
         "updated_at": datetime.utcnow()
     }
