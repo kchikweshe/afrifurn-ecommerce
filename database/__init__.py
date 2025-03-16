ad5390473766c27a15a8d56bf08d6ce186efe660
Komborerai Chikweshe <kombogc@gmail.com>
2025-03-16 15:16:22 +0200

    add production environment

--- database/__init__.py
+++ database/__init__.py
@@ -18,10 +18,15 @@ try:
     MONGO_PORT = get_required_env("MONGO_PORT", "27017")
     MONGO_DB_NAME = get_required_env("MONGO_DB_NAME", "afrifurn")
     MONGO_USER = get_required_env("MONGO_USER", "afrifurn")
-    MONGO_PASSWORD = get_required_env("MONGODB_PASSWORD", "mypasswordi")  # No default for password
-
+    MONGO_PASSWORD = get_required_env("MONGODB_PASSWORD", "mypassword")  # No default for password
+    print(f"=================== MONGO_PASSWORD: {MONGO_PASSWORD} =====================")
+    print(f"=================== MONGO_HOST: {MONGO_HOST} =====================")
+    print(f"=================== MONGO_PORT: {MONGO_PORT} =====================")
+    print(f"=================== MONGO_DB_NAME: {MONGO_DB_NAME} =====================")
+    print(f"=================== MONGO_USER: {MONGO_USER} =====================")
     # Construct MongoDB URI without logging credentials
     MONGO_URI = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB_NAME}"
+    logging.info(f"=================== MONGO_URI: {MONGO_URI} =====================")
 
     client = MongoClient(MONGO_URI)
     db = client.get_database(MONGO_DB_NAME)
