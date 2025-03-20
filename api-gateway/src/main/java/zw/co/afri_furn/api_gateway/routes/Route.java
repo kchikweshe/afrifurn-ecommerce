f160a84f5a37f2e3c1bd6c4e0dd49c6e77d832e6
Komborerai Chikweshe <kombogc@gmail.com>
2025-03-20 21:47:52 +0200

    resolve urls to afri-furn

--- api-gateway/src/main/java/zw/co/afri_furn/api_gateway/routes/Route.java
+++ api-gateway/src/main/java/zw/co/afri_furn/api_gateway/routes/Route.java
@@ -13,14 +13,14 @@ public class Route {
 
         @Bean
         public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-                String hostIp = "afrifurn-product-service";
-                String orderIP = "afrifurn-product-service";
+                String hostIp = "www.afri-furn.co.zw";
+                String orderIP = "www.afri-furn.co.zw";
 
                 return builder.routes()
                                 .route("auth-service", r -> r.path("/auth")
                                                 .uri("http://" + hostIp + ":8002/auth/api/v1/**"))
                                 .route("product-service", r -> r.path("/product-service/api/v1/**")
-                                                .uri("http://" + hostIp + ":80/api/v1/**"))
+                                                .uri("http://" + hostIp + ":8000/api/v1/**"))
                                 .route("order-service", r -> r.path("/order")
                                                 .uri("http://" + orderIP + ":8004/"))
                                 .build();
