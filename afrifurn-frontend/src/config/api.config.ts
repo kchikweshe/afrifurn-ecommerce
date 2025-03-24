3384c4bf4cb5a1b12acb4044ed2e5879133bd40a
Komborerai Chikweshe <kombogc@gmail.com>
2025-03-24 21:32:38 +0200

    change to docker host name and remove https

--- afrifurn-frontend/src/config/api.config.ts
+++ afrifurn-frontend/src/config/api.config.ts
@@ -1,7 +1,7 @@
 import { HOST_IP } from "@/data/urls";
 import axios from "axios";
 
-export const API_GATEWAY = `https://${HOST_IP}:8090`;
+export const API_GATEWAY = `http://${HOST_IP}:8090`;
 export const api_version:string="/api/v1"
 
 
