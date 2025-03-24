c9b80ce19f5d2f02edce73b4a3a796f68ac7aa1a
Komborerai Chikweshe <kombogc@gmail.com>
2025-03-24 22:01:34 +0200

    change to docker host name and remove https

--- afrifurn-frontend/src/config/api.config.ts
+++ afrifurn-frontend/src/config/api.config.ts
@@ -1,25 +1,31 @@
 import { HOST_IP } from "@/data/urls";
 import axios from "axios";
 
-export const API_GATEWAY = `http://${HOST_IP}:8090`;
+export const API_GATEWAY = `https://${HOST_IP}`;
 export const api_version:string="/api/v1"
 
 
   
 export const authService= axios.create({
     baseURL: `${API_GATEWAY}/auth${api_version}`,  
-    headers: {"Access-Control-Allow-Origin": "*"}
-
-  });
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+    }
+});
   
 export const productMicroService= axios.create({
     baseURL: `${API_GATEWAY}/product-service${api_version}`,
-    headers: {"Access-Control-Allow-Origin": "*"}
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+    }
+});
 
-  });
-
- export const orderMicroService= axios.create({
+export const orderMicroService= axios.create({
     baseURL: `${API_GATEWAY}/orders${api_version}`,
-    headers: {"Access-Control-Allow-Origin": "*"}
-
-  });
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+    }
+});
