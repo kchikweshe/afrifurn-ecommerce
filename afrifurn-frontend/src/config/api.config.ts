7304b59b18f17465e45645c5425fda7936876bbb
Komborerai Chikweshe <kombogc@gmail.com>
2025-04-28 11:12:18 +0200

    change url to https

--- afrifurn-frontend/src/config/api.config.ts
+++ afrifurn-frontend/src/config/api.config.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Use the actual domain instead of HOST_IP for production
 export const API_GATEWAY = process.env.NODE_ENV === 'production' 
-  ? 'https://afrifurn.co.zw/api'
+  ? 'https://afri-furn.co.zw/api'
   : `http://${HOST_IP}`;
 export const api_version:string="/api/v1"
 
