d635f24e5d039ed71140eb69265ac04557b0fc7c
Komborerai Chikweshe <kombogc@gmail.com>
2025-05-27 21:07:56 +0200

    add production urls

--- afrifurn-frontend/src/data/urls.ts
+++ afrifurn-frontend/src/data/urls.ts
@@ -1,5 +1,5 @@
 console.info(`============================HOST_IP:${process.env.NEXT_PUBLIC_HOST_IP}===========================`)
 export const HOST_IP=process.env.NEXT_PUBLIC_HOST_IP||'afri-furn.co.zw:8090'
-export const PUBLIC_URL=process.env.NEXT_PUBLIC_URL
-export const PRODUCT_IMAGE_URLS=process.env.NEXT_PUBLIC_PRODUCT_IMAGES_URL
+export const PUBLIC_URL=process.env.NEXT_PUBLIC_URL||"http://0.0.0.0:3000"
+export const PRODUCT_IMAGE_URLS=process.env.NEXT_PUBLIC_PRODUCT_IMAGES_URL  ||"http://afri-furn.co.zw:8000"
 
