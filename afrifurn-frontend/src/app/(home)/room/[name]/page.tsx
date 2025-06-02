d7d4151218b57cafcabba13d8fbdeeb999d127fe
Komborerai Chikweshe <kombogc@gmail.com>
2025-06-02 14:31:38 +0200

    remove unneccessary code

--- afrifurn-frontend/src/app/(home)/room/[name]/page.tsx
+++ afrifurn-frontend/src/app/(home)/room/[name]/page.tsx
@@ -9,7 +9,5 @@ export default async function Page(props: { params: Promise<{ name: string }> })
 
     // fetch category
     const level2Categories = await categoryService.getLevel2CategoriesByShortName(params.name)
-    console.log("Level 2: ", level2Categories)
-    if(!level2Categories){return}
     return <FurniturePage shortName={params.name} title={params.name.toUpperCase().replace('-', ' ')} categories={level2Categories} />
 }
